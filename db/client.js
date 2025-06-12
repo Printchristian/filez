daf8c3298896b274fdd25994eb0ec04a383b9625
christian calderon <print.christiancalderon@gmail.com>
2025-06-11 23:52:06 -0700

    done

--- db/client.js
+++ db/client.js
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config(); // ✅ Must come BEFORE using process.env
+
 import pg from "pg";
+
 const db = new pg.Client(process.env.DATABASE_URL);
+
 export default db;
